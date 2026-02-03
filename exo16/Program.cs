d69384ed644308194cc71a683e77a0a7c0a2733c
anthonyé <coquerelanthony1@gmail.com>
2026-02-03 11:13:09 +0100

    exo 16

---
+++ exo16/Program.cs
@@ -0,0 +1,24 @@
+﻿List<string> listeDeString = new List<string>();
+
+
+Console.WriteLine("Choisir un mot a ajouter");
+Console.WriteLine("écrire stop pour arreter");
+
+
+
+
+while (true)
+{
+    string? mot = Console.ReadLine();
+    if (mot == "stop")
+    {
+        break;
+    }
+    else listeDeString.Add(mot);
+}
+
+Console.WriteLine(listeDeString.Count);
+foreach (string str  in listeDeString)
+{
+    Console.Write(str);
+}
