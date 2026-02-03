7e4bae281c326b82528eaa99c1677dbe8d1f3c93
anthonyé <coquerelanthony1@gmail.com>
2026-02-03 11:23:00 +0100

    ajout exo 17

---
+++ exo 17/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+List<int> tempe = new List<int>();
+int cptInvalide = 0;
+
+
+Console.WriteLine("entrer des température");
+Console.WriteLine("-999 pour sortir");
+
+while (true)
+{
+   
+    int temperature = int.Parse(Console.ReadLine());
+    if ( temperature != -999) 
+    {
+        
+        
+        if (temperature <= 50 && temperature >= -50)
+        {
+            tempe.Add(temperature);
+
+        }
+        else
+        {
+            cptInvalide++;
+        }
+    }
+    else 
+    {
+        break;
+    }
+
+}
+
+tempe.Sort();
+//valide
+Console.WriteLine(tempe.Count);
+//invalide
+Console.WriteLine(cptInvalide);
+//temperature max
+Console.WriteLine(tempe[tempe.Count - 1]);
+//temparature min
+Console.WriteLine(tempe[0]);
