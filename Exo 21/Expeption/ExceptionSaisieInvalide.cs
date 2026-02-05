3c7e26a95185a6e853dcbb05b6ca4ec8b92da716
anthonyé <coquerelanthony1@gmail.com>
2026-02-05 13:39:47 +0100

    ajout exo21

---
+++ Exo 21/Expeption/ExceptionSaisieInvalide.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_21.Expeption
+{
+    internal class ExceptionSaisieInvalide : Exception
+    {
+
+        public ExceptionSaisieInvalide(string name):base(name)
+        {
+        
+        
+        }
+    }
+}
---
+++ Exo 21/Program.cs
@@ -0,0 +1,33 @@
+﻿using Exo_21.Expeption;
+
+int[] tabl = new int[5];
+tabl[0] = 10;
+tabl[1] = 18;
+tabl[2] = 5;
+tabl[3] = 1;
+tabl[4] = -2;
+
+ static double calculMoyenn (int[] table)
+{
+    if (table == null || table.Length == 0)
+        throw new ExceptionSaisieInvalide("Le tableau ne peux pas être vide");
+
+    foreach(int i in table)
+    {
+        if (i < 0 || i > 20)
+        {
+            throw new ArgumentOutOfRangeException(nameof(table));
+        }
+    }
+    return table.Average();
+}
+
+try
+{
+   double moyenne = calculMoyenn(tabl);
+    Console.WriteLine($"La moyenne est de {moyenne}");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
