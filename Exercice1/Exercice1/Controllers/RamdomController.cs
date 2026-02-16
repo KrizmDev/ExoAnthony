86fe69b8fc3843ec1c5af66a5fcf899b5cfb706e
anthonyé <coquerelanthony1@gmail.com>
2026-02-16 12:06:27 +0100

    Exo 1 MVC

---
+++ Exercice1/Exercice1/Controllers/RamdomController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercice1.Controllers
+{
+    public class RamdomController : Controller
+    {
+        public IActionResult index()
+        {
+            Random rdn = new Random();
+            int nombre = rdn.Next(1,101);
+            
+
+            ViewData["Nombre_aléatoire"] = nombre;
+
+            return View();
+        }
+    }
+}
